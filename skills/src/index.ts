1b5a23105bb379732af8c490df87e69244ba52db
areeba-if <areeba.iftikhar1993@gmail.com>
2022-02-14 22:58:44 -0600

    initial-setup-dependency-installation

---
+++ skills/src/index.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
+
+// environemnt variables defined in kubernetes deployments
+import 'dotenv/config.js';
