70984419175515d32c9be8093648843219b71cfd
areeba-if <areeba.iftikhar1993@gmail.com>
2022-02-14 23:15:42 -0600

    express-server-running

--- skills/src/index.ts
+++ skills/src/index.ts
@@ -5,3 +5,37 @@ import swaggerJSDoc from 'swagger-jsdoc';
 
 // environemnt variables defined in kubernetes deployments
 import 'dotenv/config.js';
+
+// inside module imports
+import { skillRouter } from './routes/skills';
+
+const PORT = 4000;
+
+const startServer = async () => {
+    try {
+        const app = express();
+        app.set('trust proxy', true);
+
+        //middleware
+        app.use(bodyParser.json());
+
+        // api-documentation
+        //  app.use(
+        //     '/api/todo/task-docs',
+        //     swaggerUi.serve,
+        //     swaggerUi.setup(swaggerDocument)
+        // );
+        app.use(skillRouter);
+        // error-handler
+        //app.use(errorHandler);
+
+        //listen on port
+        app.listen(PORT, () => {
+            console.log(`skill service running on ${PORT}`);
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+startServer();
---
+++ skills/src/routes/skills.ts
@@ -0,0 +1,9 @@
+import express, { NextFunction, Response, Request } from 'express';
+
+const router = express.Router();
+
+router.get('/api/skills', (req: Request, res: Response) => {
+    res.status(200).send('helloworld');
+});
+
+export { router as skillRouter };
